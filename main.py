48935d5301fbcd66f21c306bb6203b9b93b864e0
skdn <sakuden542+git@gmail.com>
2017-12-16 02:51:02 +0900

    continualing

--- main.py
+++ main.py
@@ -2,18 +2,39 @@
 # -*- coding: utf-8 -*-
 """Flask."""
 
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from GetStatus import GetStatus
 from SetStatus import SetStatus
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 
 @app.route('/')
 def index():
   """Index."""
-  return "Hello, World!"
+  return redirect('/static/index.html')
+
+@app.route('/main.css')
+def main_css():
+  """Index."""
+  return redirect('/static/main.css')
+
+@app.route('/script.js')
+def script_js():
+  """Index."""
+  return redirect('/static/script.js')
+
+@app.route('/skdn.jpg')
+def skdn_jpg():
+  """Index."""
+  return redirect('/static/skdn.jpg')
+
+@app.route('/edge-icons-Regular.woff')
+def font_file():
+  """Index."""
+  return redirect('/static/edge-icons-Regular.woff')
+
 
 @app.route('/getStatus')
 def getStatus():
@@ -21,7 +42,8 @@ def getStatus():
   g = GetStatus()
   return g.getStatus()
 
-@app.route('/setStatus')
+
+@app.route('/setStatus', methods=['POST'])
 def setStatus():
   """Set Status."""
   s = SetStatus()
